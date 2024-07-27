2ac0fcde22b10c8b312c94f7981c6b969e45d4f2
Aisha Hasanguliyeva <ahasanguliyeva02@gmail.com>
2024-07-27 13:10:51 +0400

    login register ui

---
+++ AstroTalk/Scene/LoginController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginController.swift
+//  AstroTalk
+//
+//  Created by Aisha on 27.07.24.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
